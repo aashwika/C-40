fdea9cf403b695d61d10f524ab6c80533ba0005c
Your Name <you@example.com>
2020-08-26 19:23:57 +0530

    final

--- js/Game.js
+++ js/Game.js
@@ -43,6 +43,8 @@ class Game {
   play(){
     form.hide();
     
+    player.getCarsAtEnd();
+
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
@@ -93,6 +95,8 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank += 1
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +104,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
